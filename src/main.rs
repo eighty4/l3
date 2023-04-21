4a015c1b58b19bc032ad82b053291d2180e47c4d
Adam McKee <adam.be.g84d@gmail.com>
2023-04-20 22:15:15 -0500

    calc sha256 checksum of a file

--- src/main.rs
+++ src/main.rs
@@ -21,3 +21,39 @@ async fn main() -> Result<(), Error> {
 
     Ok(())
 }
+
+#[allow(dead_code)]
+fn sha256_checksum(path: &PathBuf) -> Result<String, Error> {
+    let mut hasher = Sha256::new();
+    let mut file = fs::File::open(path)?;
+    io::copy(&mut file, &mut hasher)?;
+    let hash_bytes = hasher.finalize();
+    Ok(base64::engine::general_purpose::STANDARD.encode(hash_bytes))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use io::Write;
+    use temp_dir::TempDir;
+
+    #[test]
+    fn test() {
+        let d = TempDir::new().expect("make temp dir");
+        let p = d.path().join("file");
+        let mut f = fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open(&p)
+            .expect("create file");
+        f.write_all("content".as_bytes())
+            .expect("write bytes to file");
+
+        let result = sha256_checksum(&p);
+        assert!(result.is_ok());
+        assert_eq!(
+            "7XACtDnprIRfIjV9giusFERzD722AW0+yUMil7nsn3M=",
+            result.unwrap()
+        );
+    }
+}
