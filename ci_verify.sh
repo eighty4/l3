91b0ea00d77c92dd851f3e74151d19cb17bed115
Adam McKee <adam.be.g84d@gmail.com>
2024-06-02 18:17:31 -0500

    ci_verify.sh fails if cargo fmt updates sources

--- ci_verify.sh
+++ ci_verify.sh
@@ -3,7 +3,13 @@ set -e
 
 # run through all the checks done for ci
 
+if [ -n "$(git status --porcelain)" ]; then
+  echo "error: run \`ci_verify.sh\` with a clean working directory"
+  exit 1
+fi
+
 cargo fmt -v
+git diff --exit-code
 cargo clippy -- -D warnings
 cargo build
 cargo test
