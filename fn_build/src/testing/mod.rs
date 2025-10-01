e23bbd9777cb49827b33fc3d29be39e1139221ae
Adam McKee <adam.be.g84d@gmail.com>
2025-10-01 17:46:34 -0500

    refactor overly complicated test fixtures

--- fn_build/src/testing/mod.rs
+++ fn_build/src/testing/mod.rs
@@ -11,42 +11,52 @@ use std::path::{Path, PathBuf};
 
 pub use utilities::*;
 
-#[allow(unused)]
-enum FixtureTestMode {
-    AllFixtures(PathBuf),
-    SingleFixture(PathBuf),
-}
-
+// run with `cargo test test_single_fixture -- -- --fixture fixtures/node/ts/import_uses_js`
 #[tokio::test]
 async fn test_single_fixture() {
-    for fixture in collect_fixtures(FixtureTestMode::SingleFixture(PathBuf::from(
-        "fixtures/node/ts/import_uses_js",
-    )))
-    .into_iter()
-    .map(create_fixture)
-    {
+    let mut fixture: Option<PathBuf> = None;
+    let mut args = std::env::args().into_iter();
+    while let Some(arg) = args.next() {
+        if arg.as_str() == "--fixture" {
+            match args.next().map(PathBuf::from) {
+                Some(mut maybe_fixture) => {
+                    if !maybe_fixture.is_absolute() && !maybe_fixture.starts_with("fixtures") {
+                        maybe_fixture = PathBuf::from("fixtures").join(maybe_fixture);
+                    }
+                    if !maybe_fixture.is_dir() {
+                        panic!(
+                            "`--fixture {}` is not a directory",
+                            maybe_fixture.to_string_lossy()
+                        );
+                    }
+                    if !is_fixture_dir(&maybe_fixture) {
+                        panic!(
+                            "`--fixture {}` is not a fixture",
+                            maybe_fixture.to_string_lossy()
+                        );
+                    }
+                    fixture = Some(maybe_fixture);
+                    break;
+                }
+                None => panic!("--fixture used without value"),
+            }
+        }
+    }
+    if let Some(fixture) = fixture.map(create_fixture) {
         fixture.run().await;
     }
 }
 
 #[tokio::test]
 async fn test_node_fixtures() {
-    for fixture in collect_fixtures(FixtureTestMode::AllFixtures(PathBuf::from("fixtures/node")))
-        .into_iter()
-        .map(create_fixture)
-    {
+    for fixture in collect_fixtures("fixtures/node", false) {
         fixture.run().await;
     }
 }
 
 #[tokio::test]
 async fn test_python_fixtures() {
-    for fixture in collect_fixtures(FixtureTestMode::AllFixtures(PathBuf::from(
-        "fixtures/python",
-    )))
-    .into_iter()
-    .map(create_fixture)
-    {
+    for fixture in collect_fixtures("fixtures/python", false) {
         fixture.run().await;
     }
 }
@@ -54,10 +64,7 @@ async fn test_python_fixtures() {
 #[tokio::test]
 #[ignore]
 async fn update_node_gold() {
-    for fixture in collect_fixtures(FixtureTestMode::AllFixtures(PathBuf::from("fixtures/node")))
-        .into_iter()
-        .map(create_update_gold_fixture)
-    {
+    for fixture in collect_fixtures("fixtures/node", true) {
         fixture.run().await;
     }
 }
@@ -65,23 +72,21 @@ async fn update_node_gold() {
 #[tokio::test]
 #[ignore]
 async fn update_python_gold() {
-    for fixture in collect_fixtures(FixtureTestMode::AllFixtures(PathBuf::from(
-        "fixtures/python",
-    )))
-    .into_iter()
-    .map(create_update_gold_fixture)
-    {
+    for fixture in collect_fixtures("fixtures/python", true) {
         fixture.run().await;
     }
 }
 
-fn collect_fixtures(mode: FixtureTestMode) -> Vec<PathBuf> {
-    match mode {
-        FixtureTestMode::AllFixtures(p) => {
-            collect_fixture_dirs(&env::current_dir().unwrap().join(p)).unwrap()
-        }
-        FixtureTestMode::SingleFixture(p) => vec![env::current_dir().unwrap().join(p)],
-    }
+fn collect_fixtures(p: &str, update_gold: bool) -> Vec<TestFixture> {
+    collect_fixture_dirs(&env::current_dir().unwrap().join(p))
+        .unwrap()
+        .into_iter()
+        .map(if update_gold {
+            create_update_gold_fixture
+        } else {
+            create_fixture
+        })
+        .collect()
 }
 
 pub fn create_fixture(fixture_dir: PathBuf) -> TestFixture {
@@ -98,10 +103,12 @@ fn collect_fixture_dirs(p: &Path) -> Result<Vec<PathBuf>, anyhow::Error> {
         let dir_entry = dir_entry_result?;
         let p = dir_entry.path();
         if p.is_dir() {
-            if is_fixture_dir(&p) {
-                fixture_dirs.push(p);
-            } else {
-                fixture_dirs.append(&mut collect_fixture_dirs(&p)?);
+            if !is_excluded_fixture_dir(&p) {
+                if is_fixture_dir(&p) {
+                    fixture_dirs.push(p);
+                } else {
+                    fixture_dirs.append(&mut collect_fixture_dirs(&p)?);
+                }
             }
         }
     }
@@ -109,7 +116,7 @@ fn collect_fixture_dirs(p: &Path) -> Result<Vec<PathBuf>, anyhow::Error> {
 }
 
 fn is_fixture_dir(p: &Path) -> bool {
-    p.join(".fixture").is_dir() && !is_excluded_fixture_dir(p)
+    p.join(".fixture").is_dir()
 }
 
 fn is_excluded_fixture_dir(p: &Path) -> bool {
